673509a07dc847f59576a25bd8707669cd223bb1
shwetasingh89 <46380444+shwetasingh89@users.noreply.github.com>
2019-02-28 20:28:54 +0530

    add project

---
+++ maven/src/test/java/com/maven/selenium/TestingMaven.java
@@ -0,0 +1,15 @@
+package com.maven.selenium;
+
+import org.testng.annotations.Test;
+
+public class TestingMaven 
+{
+	
+    @Test
+	public static void main(String[] args)
+    {
+		System.out.println("Welcome to maven");
+
+	}
+
+}
